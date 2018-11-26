d874e76f2af6ab1e9e4b7dadda82fa179e30f8b4
T-Zema <zema.tomasz@gmail.com>
2018-11-26 13:04:38 +0100

    Just saved last activity in project

--- Helwiq.R
+++ Helwiq.R
@@ -3,19 +3,19 @@ library("sp")
 library("RColorBrewer")
 # Czytanie pliku
 #df <- read.table(".\\Rysunek\\Doktorat_obliczenia_EU-28_2006.csv", header = TRUE)
-df <- read.csv(".\\Rysunek\\Doktorat_obliczenia_EU-28_2006.csv", header = TRUE)
+df <- read.csv(".\\Rysunek\\Doktorat_obliczenia_EU-28_2016.csv", header = TRUE)
 # zmienne pomocnicze
 lista <- 0:28
 x <- c(0,df$zi)
-x[1] <- col("white")
+
 y = lista
 Średnia = df$sr[1]
 Punkt1 = df$p1[1] 
 Punkt2 = df$p2[1]
 
 # plotowanie
-plot(x, y, col=ifelse(x==0, "white", "blue"), xlab="zi", ylab="Lista państw ", pch=19, xaxt="n", yaxt="n")
-  title("Metoda Hellwiga", line = +2)
+plot(x, y, col=ifelse(x==0, "white", "blue"), xlab="wartość syntetycznego miernika ekorozowju", ylab="Pozycja w rankingu ", pch=19, xaxt="n", yaxt="n")
+
 
 
   # zmienna pomocnicza
@@ -28,10 +28,9 @@ plot(x, y, col=ifelse(x==0, "white", "blue"), xlab="zi", ylab="Lista państw ", 
   abline(v = Średnia, col = "gray60", lwd = 2, lty = 2)
   abline(v = Punkt1, col = "gray60", lwd = 2, lty = 2)
   abline(v = Punkt2, col = "gray60", lwd = 2, lty = 2)
+  
   # Znaczniki dla linii
-  # dla roku 
-
-  mtext(expression(bar("Z") -"S"[d]), line = 0, adj = Punkt1+0.01)
-  mtext(expression(bar("Z") + "S"[d]), line = 0, adj = Punkt2+0.02)
-  mtext(expression(bar("Z")), line = 0.2, adj = Średnia+0.02)
+  mtext(expression(bar("Z") -"S"[d]), line = 0, at = Punkt1)
+  mtext(expression(bar("Z") + "S"[d]), line = 0, at = Punkt2)
+  mtext(expression(bar("Z")), line = 0.2, at = Średnia)
   
