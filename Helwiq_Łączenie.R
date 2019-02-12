cd2250acbc9ceba2212060d411eb50f0e378ad61
T-Zema <tomasz.zema@gmail.com>
2019-02-12 18:19:13 +0100

    2019 update

--- Helwiq_Łączenie.R
+++ Helwiq_Łączenie.R
@@ -1,24 +1,24 @@
-# deklaracja biblioteki "sp"
-library("sp")
+# deklaracja biblioteki 
+library("stringr")
 # hotfix do polskiej czcionki
-encoding = "utf-8"
+
 Sys.setlocale("LC_CTYPE", "polish")
 # Czytanie pliku
-df <- read.csv(".\\Arkusze\\Rankingi csv\\Doktorat_obliczenia_EU-28_2006.csv", header = TRUE)
+df <- read.csv(".\\Arkusze\\Rankingi csv\\Doktorat_obliczenia_EU-28_2006_wzorzec.csv", header = TRUE, encoding = "UTF-8")
 
 # zmienne pomocnicze
 lista <- 0:28
 x <- c(0,df$zi)
-
+x2 <- c(0,df2$zi)
 y = lista
 Średnia = df$sr[1]
 Punkt1 = df$p1[1] 
 Punkt2 = df$p2[1]
 
 # plotowanie
-plot(x, y, col=ifelse(x==0, "white", "blue"), xlab="Wartość syntetycznego miernika ekorozowju", ylab="Pozycja w rankingu ", pch=19, xaxt="n", yaxt="n")
-
-
+plot(x, y, col=ifelse(x==0, "white", "blue"), xlab="Wartość syntetycznego miernika ekorozowju", ylab="Pozycja w rankingu ", pch=19, xaxt="n", yaxt="n",cex= 2)
+#plot(x2, y, col=ifelse(x2==0, "white", "red"), xlab="Wartość syntetycznego miernika ekorozowju", ylab="Pozycja w rankingu ", pch=19, xaxt="n", yaxt="n")
+points(x2,y, col=ifelse(x==0, "white", "red"),cex= 2)
 
   # zmienna pomocnicza
   axis_seqx <- seq(0, 1, length.out = 11)
@@ -44,8 +44,8 @@ plot(x, y, col=ifelse(x==0, "white", "blue"), xlab="Wartość syntetycznego mier
   linear_model = lm(y~x,data=df)
   
   #abline(linear_model)
-  text(x[2:29],y[2:29], labels=df$symbolh, cex= 0.5, pos=2.5)
-  
+  text(x[2:29]-(0.05),y[2:29], labels=df$symbolh, cex= 0.7, pos=4)
+  text(x2[2:29]*1.01,y[2:29], labels=df2$symbolh, cex= 0.7,  pos=4, col = "red")
   summary(linear_model)
   summary(linear_model$coefficients)
   
