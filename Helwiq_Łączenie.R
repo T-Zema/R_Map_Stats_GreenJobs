24ec21b67558e1d027205515a65d9ac58bd57982
T-Zema <tomasz.zema@gmail.com>
2019-02-13 18:28:22 +0100

    The end? Finished examples

--- Helwiq_Łączenie.R
+++ Helwiq_Łączenie.R
@@ -5,12 +5,14 @@ library("stringr")
 
 Sys.setlocale("LC_CTYPE", "polish")
 # Czytanie pliku
-df <- read.csv(".\\Arkusze\\Rankingi csv\\Doktorat_obliczenia_EU-28_2006_wzorzec.csv", header = TRUE, encoding = "UTF-8")
+plik = "./Arkusze/Rankingi csv/Doktorat_obliczenia_EU-28_mediana_wzorzec.csv"
+df <- read.csv(plik, header = TRUE, encoding = "UTF-8")
 
 # zmienne pomocnicze
 lista <- 0:28
 x <- c(0,df$zi.1[1:28])
-x2 <- c(0,df$Patenty.związane.z.recyklingiem.i.surowcami.wtórnymi...liczba.[1:28])
+#x2 <- c(0,df$Patenty.związane.z.recyklingiem.i.surowcami.wtórnymi...liczba.[1:28]) #patenty
+x2 <- c(0,df$Krajowa.konsumpcja.biomasy..100.tys..ton.ekwiwalentu.oleju.[1:28]) 
 y = lista
 Średnia = mean(df$zi.1[1:28])
 Odch.st = sd(df$zi.1[1:28])
@@ -28,6 +30,13 @@ Państwa %<>%
      gsub("Niemcy", "DE", .) %>% gsub("Polska", "PL", .) %>% gsub("Portugalia", "PO", .) %>% gsub("Rumunia", "RO", .) %>%
      gsub("Słowenia", "SI", .) %>% gsub("Węgry", "HU", .) %>% gsub("Wielka Brytania", "GB", .) %>% gsub("Włochy", "IT", .)
 Państwa <- Państwa[1:28]
+
+regexy <- gsub(".csv", "", plik) %>% gsub("./Arkusze/Rankingi/", "", .)
+
+png(filename = paste0("./Wyniki/Wzorzec_vs_Patenty/", regexy, ".png"),
+    width = 1000, height = 770, units = "px")
+
+
 # plotowanie
 plot(x, y, col=ifelse(x==0, "white", "blue"), xlab="Wartość syntetycznego miernika ekorozowju", ylab="Pozycja w rankingu ", pch=19, xaxt="n", yaxt="n",cex= 2)
 #plot(x2, y, col=ifelse(x2==0, "white", "red"), xlab="Wartość syntetycznego miernika ekorozowju", ylab="Pozycja w rankingu ", pch=19, xaxt="n", yaxt="n")
@@ -36,7 +45,7 @@ points(x2,y, col=ifelse(x==0, "white", "red"),cex= 2)
   # zmienna pomocnicza
   axis_seqx <- seq(0, 1, length.out = 11)
   axis_seqy <- seq(0, 28, length.out = 29)
-  title("wyniki wzorca vs patenty, czerwone to Patenty")
+  title("wyniki wzorca vs biomasa, czerwone to biomasa")
   # zmiana osi
   axis(side=1,at=axis_seqx,lwd=1)
   axis(side=2,at=axis_seqy,lwd=1)
@@ -54,11 +63,15 @@ points(x2,y, col=ifelse(x==0, "white", "red"),cex= 2)
   #scatter.smooth(x[1:29], y[1:29])
   # regresja liniowa
   
+  legend("bottomleft", legend=c("Biomasa", "Mediana"),
+         col=c("red", "blue"),bty = "n", pch = c(16,16), cex=0.9)
+  
   linear_model = lm(y~x,data=df)
   
   #abline(linear_model)
   text(x[2:29],y[2:29], labels=Państwa, cex= 0.7, pos=3)
   text(x2[2:29],y[2:29], labels=Państwa, cex= 0.7,  pos=3, col = "red")
-  summary(linear_model)
-  summary(linear_model$coefficients)
+ 
+  
+  dev.off()
   
